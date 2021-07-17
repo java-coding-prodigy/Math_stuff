704aca8fd93f9c0bc16b35d5f279b823fb0f3b9e
java-coding-prodigy <85149944+java-coding-prodigy@users.noreply.github.com>
2021-07-17 08:41:49 +0530

    Made it so that you convert from Polar form to regular as well

--- src/io/github/javacodingprodigy/mathstuff2/numberstuff/polarFormConverter.java
+++ src/io/github/javacodingprodigy/mathstuff2/numberstuff/polarFormConverter.java
@@ -1,20 +1,36 @@
 package io.github.javacodingprodigy.mathstuff2.numberstuff;
 
 import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
+
+import static java.lang.Math.*;
+import static io.github.javacodingprodigy.mathstuff2.numberstuff.Approx.*;
 
 public class polarFormConverter {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double x = sc.nextDouble();
-        double y = sc.nextDouble();
-        System.out.println(x + "" + String.format("%+f", y) + "i = " + radius(x, y) + "* e^i" + theta(x, y));
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		System.out.println(x + "" + String.format("%+f", y) + "i = " + radius(x, y) + "* e^" + theta(x, y) + 'i');
+		double radius = sc.nextDouble();
+		double theta = toRadians(sc.nextDouble());
+		System.out.println(radius + "*" + "e^" + theta + "i = " + x(radius, theta) + ' ' + String.format("%+f", y(radius, theta)) + 'i');
+	}
 
-    public static double radius(double x, double y) {
-        return Math.sqrt(x * x + y * y);
-    }
+	public static double radius(double x, double y) {
+		return Math.sqrt(x * x + y * y);
+	}
 
-    public static double theta(double x, double y) {
-        return Math.atan2(x, y);
-    }
+	public static double theta(double x, double y) {
+		return Math.atan2(x, y);
+	}
+
+	public static double x(double radius, double theta) {
+		return approx(radius * cos(theta));
+	}
+
+	public static double y(double radius, double theta) {
+		return radius * sin(theta);
+	}
+
 }
